b823a6373cd458f6f5cce0ed4f2d8ffb7eff9fa6
William Infante <william.infante@iress.com>
2020-12-21 20:24:36 +1100

    Update script with png and different naming convention

--- photos_multifolder.sh
+++ photos_multifolder.sh
@@ -6,39 +6,49 @@
 #   cd ~/Desktop
 #   ~/dev/photos_workflow.sh
 # Expected: '2020.01.01 Folder Name' is a folder in ~/Desktop.
-#           '2020.01.01 Folder Name' has an image file '1.jpg' and '2.jpg'
-#           '1.jpg' and '2.jpg' will have the folder name in their metadata
+#           '2020.01.01 Folder Name' has an image file '1.jpg' and '2.jpeg'
+#           '1.jpg' and '2.jpeg' will have the folder name in their metadata
+
+fnc_overwrite() {
+    echo "DIRECTORY: $(basename "$d")"
+    echo "FILENAME: $f"
+    echo "overwriting title/description"
+    exiftool \
+        -overwrite_original \
+        -XMP:Title="$NEW_TITLE" \
+        -EXIF:ImageDescription="$NEW_TITLE" \
+        -XMP:Description="$NEW_TITLE" \
+        -EXIF:XPTitle="$NEW_TITLE" \
+        "$f"
+
+    # display what we've set
+    exiftool -f \
+        -XMP:Title \
+        -EXIF:ImageDescription \
+        -XMP:Description \
+        -EXIF:XPTitle \
+        "$f"
+}
 
 for d in */; do
-    echo $d 
-        for f in "$d"*.jpg; do
-            echo "$f"
-            echo $(basename "$d")
-            #NEW_TITLE="${PWD##*/}"
-            NEW_TITLE="${d%?}"
-            OLD_TITLE="$(exiftool -s -s -s -XMP:Title "$f")"
-            # Update
-            if [ "$OLD_TITLE" != "$NEW_TITLE" ]; then
-                echo "overwriting title"
-                exiftool \
-                    -overwrite_original \
-                    -XMP:Title="$NEW_TITLE" \
-                    -EXIF:ImageDescription="$NEW_TITLE" \
-                    -IPTC:Caption-Abstract="$NEW_TITLE" \
-                    -XMP:Description="$NEW_TITLE" \
-                    -EXIF:XPTitle="$NEW_TITLE" \
-                    "$f"
+    shopt -s nullglob
+    for f in "$d"*.jp*g "$d"*.png; do
+        NEW_TITLE="${d%?}"
+        OLD_TITLE="$(exiftool -s -s -s -EXIF:ImageDescription "$f")"
 
-                # display what we've set
-                exiftool -f \
-                    -XMP:Title \
-                    -EXIF:ImageDescription \
-                    -IPTC:Caption-Abstract \
-                    -XMP:Description \
-                    -EXIF:XPTitle \
-                    "$f"
+        if [ "$OLD_TITLE" == "" ]; then
+            fnc_overwrite
+        elif [ "$OLD_TITLE" != "$NEW_TITLE" ]; then
+            echo "DIRECTORY: $(basename "$d")"
+            echo "FILENAME: $f"
+            echo "CURRENT TITLE: $OLD_TITLE"
+            read -p "Not an empty title, do you want to rename? " -n 1 -r
+            echo
+            if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+                echo "skipping, not overrwriting incorrect title name"
             else
-                echo "skipping because correct title exists already"
+                fnc_overwrite
             fi
-   done
+        fi
+    done
 done
